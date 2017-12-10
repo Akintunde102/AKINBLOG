c0f42a1f5c529610e127b1b9d0da929b5d996791
Akintunde102 <jegedeakintunde@gmail.com>
2017-12-10 01:49:30 +0100

    Add files via upload

---
+++ admin/add-post.php
@@ -0,0 +1,111 @@
+<?php //include config
+require_once('../includes/config.php');
+
+//if not logged in redirect to login page
+if(!$user->is_logged_in()){ header('Location: login.php'); }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Admin - Add Post</title>
+  <link rel="stylesheet" href="../style/normalize.css">
+  <link rel="stylesheet" href="../style/main.css">
+	<script src="../js/ckeditor/ckeditor.js"></script>
+<script src="../js/ckeditor/samples/js/sample.js"></script>	
+	<link rel="stylesheet" href="../js/ckeditor/samples/toolbarconfigurator/lib/codemirror/neo.css">
+</head>
+<body>
+
+<div id="wrapper">
+
+	<?php include('menu.php');?>
+	<p><a href="./">Blog Admin Index</a></p>
+
+	<h2>Add Post</h2>
+
+	<?php
+
+	//if form has been submitted process it
+	if(isset($_POST['submit'])){
+
+		$_POST = array_map( 'stripslashes', $_POST );
+
+		//collect form data
+		extract($_POST);
+
+		//very basic validation
+		if($postTitle ==''){
+			$error[] = 'Please enter the title.';
+		}
+
+		if($postDesc ==''){
+			$error[] = 'Please enter the description.';
+		}
+
+		if($postCont ==''){
+			$error[] = 'Please enter the content.';
+		}
+		
+		
+		if($postTags ==''){
+			$error[] = 'Please enter the tags.';
+		}
+
+		if(!isset($error)){
+
+			try {
+
+				//insert into database
+				$stmt = $db->prepare('INSERT INTO blog_posts (postTitle,postDesc,postCont,postDate,postTags) VALUES (:postTitle, :postDesc, :postCont, :postDate, :postTags)') ;
+				$stmt->execute(array(
+					':postTitle' => $postTitle,
+					':postDesc' => $postDesc,
+					':postCont' => $postCont,
+					':postTags' => $postTags,
+					':postDate' => date('Y-m-d H:i:s')
+				));
+
+				//redirect to index page
+				header('Location: index.php?action=added');
+				exit;
+
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
+			}
+
+		}
+
+	}
+
+	//check for any errors
+	if(isset($error)){
+		foreach($error as $error){
+			echo '<p class="error">'.$error.'</p>';
+		}
+	}
+	?>
+	<form action='' method='post'>
+
+		<p><label>Title</label><br />
+		<input type='text' name='postTitle' value='<?php if(isset($error)){ echo $_POST['postTitle'];}?>'></p>
+
+		<p><label>Description</label><br />
+		<textarea name='postDesc' cols='80' rows='3'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
+
+		<p><label>Content</label><br />
+		<textarea id="editor" name='postCont' cols='60' rows='20'><?php if(isset($error)){ echo $_POST['postCont'];}?></textarea></p>
+
+		<p><label>Tags</label><br />
+		<input type='text' name='postTags' value='<?php if(isset($error)){ echo $_POST['postTags'];}?>'></p>
+
+		<p><input type='submit' name='submit' value='Submit'></p>
+
+	</form>
+
+</div>
+<script>
+	initSample();
+</script>
+</body>
+</html>	
